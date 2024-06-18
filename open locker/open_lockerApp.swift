93b2d87de1d0e2af0e0f6a13943406c527766b02
Carol Costa <anacarolalvescosta@gmail.com>
2024-06-18 15:46:11 -0300

    adiciona projeto

---
+++ open locker/open_lockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_lockerApp.swift
+//  open locker
+//
+//  Created by Ana Carolyne Alves Costa on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct open_lockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
