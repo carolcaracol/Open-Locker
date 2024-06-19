26582795dbf8b543b5208249f9c57ba805dfa88d
Carol Costa <anacarolalvescosta@gmail.com>
2024-06-19 17:12:48 -0300

    adiciona view da carta de baralho

---
+++ open locker/cartaDeBaralhoView.swift
@@ -0,0 +1,18 @@
+//
+//  cartaDeBaralhoView.swift
+//  open locker
+//
+//  Created by Ana Carolyne Alves Costa on 19/06/24.
+//
+
+import SwiftUI
+
+struct cartaDeBaralhoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cartaDeBaralhoView()
+}
