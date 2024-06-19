ef8306fe3305a5cc458431d9f05b3226366da1f8
Mateus Gonçalves <mateusg260@gmail.com>
2024-06-19 14:10:26 -0300

    Cria arquivo novo

---
+++ TelaInicial.swift
@@ -0,0 +1,24 @@
+//
+//  TelaInicial.swift
+//  open locker
+//
+//  Created by João Mateus Gonçalves da Silva on 19/06/24.
+//
+import SwiftUI
+
+struct TelaInicial: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TelaInicial()
+}
+
