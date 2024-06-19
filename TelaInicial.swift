f49a95736697fdc0187089b4200db3d57176c34e
Mateus Gonçalves <mateusg260@gmail.com>
2024-06-19 15:09:41 -0300

    Começa a ajustar a tela

--- TelaInicial.swift
+++ TelaInicial.swift
@@ -8,14 +8,32 @@ import SwiftUI
 
 struct TelaInicial: View {
     var body: some View {
-        VStack {
-            Image(.fendas)
-            Rectangle()
+        ZStack {
+            VStack {
+                Spacer()
+                ZStack {                        Image(.logoInicial)
+                    Spacer()
+                }
+                .padding(.bottom, 36.0)
+                Image(.fendas)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(.botãoIniciar)
+                    Spacer()
+                    Image(.lock)
+                        .padding()
+                }
+                Image(.sobre)
+                    .padding()
+                Spacer()
+            }
+            .padding()
+            .background(Color.roxoclaro)
+            .edgesIgnoringSafeArea(.all)
         }
-        .padding()
     }
 }
-
 #Preview {
     TelaInicial()
 }
