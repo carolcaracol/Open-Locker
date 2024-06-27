5a0260ce1298ef53831a1fd5f2881536ccf932af
Maria Julia Souza Balbino <julia.sbalbino@outlook.com>
2024-06-27 11:01:18 -0300

    Inicia teste de outra forma de navegaçao no app

---
+++ open locker/OutraView.swift
@@ -0,0 +1,31 @@
+//
+//  OutraView.swift
+//  open locker
+//
+//  Created by Maria Julia Souza Balbino on 27/06/24.
+//
+
+import SwiftUI
+
+struct OutraView: View {
+    var body: some View {
+        
+        TabView{
+            CartaDeBaralhoView()
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            SorteioView()
+                .tabItem {
+                    Image(systemName: "house")
+                    
+                }
+            
+        }
+    }
+}
+
+#Preview {
+    OutraView()
+}
--- open locker/SorteioView.swift
+++ open locker/SorteioView.swift
@@ -9,42 +9,45 @@ import SwiftUI
 
 struct SorteioView: View {
     var body: some View {
-            VStack {
-                Text("sorteio")
-                    .foregroundStyle(.purple)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .bold()
-                
-                Rectangle()
-                    .foregroundColor(.purple)
-                    .frame(width: 353, height: 10)
-                    
-                Image(.baralhoRosa)
-                    .padding(76)
-                
-                Button(
-                    action: {},
-                    label: {
-                        ZStack {
-                            Rectangle()
-                                .foregroundColor(.green)
-                                .border(Color.white)
-                                
-                            Text("SORTEAR")
-                                .foregroundStyle(.black)
-                                .bold()
-                                .font(.subheadline)
-                            
-                            
-                        }
+        VStack {
+            Text("sorteio")
+                .foregroundStyle(.purple)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+            
+            Rectangle()
+                .foregroundColor(.purple)
+                .frame(width: 353, height: 10)
+            
+            Image(.baralhoRosa)
+                .padding(76)
+            
+            Button(
+                action: {},
+                label: {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.green)
+                            .border(Color.white)
+                        
+                        Text("SORTEAR")
+                            .foregroundStyle(.black)
+                            .bold()
+                            .font(.subheadline)
+                        
+                        
                     }
-                )
-                .cornerRadius(12)
-                .frame(width: 147,height: 49)
-                
-            }
-            .padding()
+                }
+            )
+            .cornerRadius(12)
+            .frame(width: 147,height: 49)
+            
+        }
+        .padding()
+        
+        
     }
+    
 }
 
 #Preview {
